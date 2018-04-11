44510397c3717d5a22e9e35e8ca3d00c82b770ce
donrestarone <shashikejayatunge@gmail.com>
2018-04-11 18:38:34 -0400

    submit form works, root landing page works

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+	root 'pictures#index'
+	
 	get 'pictures' => 'pictures#index'
+
+	post 'pictures' => 'pictures#create'
+	get 'pictures/new' => 'pictures#new'
+
 	get 'pictures/:id' => 'pictures#show'
 end
