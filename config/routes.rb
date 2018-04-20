065041fee19449ed4273296aba5d7950aeebf802
donrestarone <shashikejayatunge@gmail.com>
2018-04-20 14:28:00 -0400

    start work on implementing authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,23 @@
 Rails.application.routes.draw do
 
+  # get 'users/new'
+
+  # get 'users/create'
+  	resources :pictures do 
+  		resource :users, only: [:new, :create]
+  	end 
+
 	root 'pictures#index'
 
-	get 'pictures' => 'pictures#index'
+	# get 'pictures' => 'pictures#index'
 
-	post 'pictures' => 'pictures#create'
-	get 'pictures/new' => 'pictures#new'
+	# post 'pictures' => 'pictures#create'
+	# get 'pictures/new' => 'pictures#new'
 
-	get 'pictures/:id' => 'pictures#show'
+	# get 'pictures/:id' => 'pictures#show'
 
-	delete 'pictures/:id' => 'pictures#destroy'
+	# delete 'pictures/:id' => 'pictures#destroy'
 
-	get 'pictures/:id/edit' => 'pictures#edit'
-	patch 'pictures/:id' => 'pictures#update'
+	# get 'pictures/:id/edit' => 'pictures#edit'
+	# patch 'pictures/:id' => 'pictures#update'
 end
